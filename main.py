d9d4757e9550b9cef68609b2ac812bc0e1802a1b
Qurqi <125725390+Qurqi@users.noreply.github.com>
2025-02-28 14:21:53 -0800

    Update main.py
    Maybe double check file saving

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Import necessary libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 from linear_regression import LinearRegressionModel
 from tests import ModelEvaluation
 from data_process import clean_data
 import numpy as np
+import tensorflow as tf
 
 
 # Load dataset
@@ -13,6 +15,11 @@ input_data = pd.read_csv('data/new_data.csv')
 cleaned_data = clean_data(input_data)
 # Split into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(cleaned_data.drop('Survived', axis=1), cleaned_data['Survived'], test_size=0.2, random_state=42)
+
+# scale data
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 # Convert train and test data into numpy arrays
 print(X_train)
 X_train = np.array(X_train, dtype=np.float32)
@@ -21,9 +28,16 @@ y_train = np.array(y_train, dtype=np.float32)
 y_test = np.array(y_test, dtype=np.float32)
 
 # Initialize and train model
-Johns_model= LinearRegressionModel(X_train.shape[1])
-Johns_model.train_model(X_train, y_train, epochs = 100, lr = 0.01)
+Mirtul_and_Johns_model= LinearRegressionModel(X_train.shape[1])
+Mirtul_and_Johns_model.train_model(X_train, y_train, epochs = 100, lr = 0.01)
+
 # Evaluate model performance
-evaluation = ModelEvaluation(Johns_model, X_test, y_test)
+evaluation = ModelEvaluation(Mirtul_and_Johns_model, X_test, y_test)
+predictions = Mirtul_and_Johns_model.predict(X_test)
+
+#Save predictions
+np.savetxt('predictions.csv', predictions, delimiter=',')
+
+# Compute and display other metrics
 mse = evaluation.evaluate_mse()
 mae = evaluation.evaluate_mae()
